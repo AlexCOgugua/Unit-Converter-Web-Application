c7f971748582d726199e0cbad4f2293a09891f1a
Alex Ogugua <154514205+AlexCOgugua@users.noreply.github.com>
2024-10-23 22:37:13 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+function c2f(){
+    let c_deg = document.getElementById("tempc").value;
+    let f_deg = document.getElementById("tempf").value;
+
+    if (c_deg !== ""){
+        f_deg = (c_deg * 9/5) + 32;
+        document.getElementById("tempf").value = f_deg;
+    }
+    else if(f_deg !== ""){
+        c_deg = (f_deg -32) * 5 / 9
+        document.getElementById("tempc").value = c_deg
+    }
+}
+
+
+
+function kg2pd(){
+    let wt_kg = document.getElementById("wtkg").value;
+    let wt_pd = document.getElementById("wtpd").value;
+
+    if (wt_kg !== ""){
+        wt_pd = (wt_kg * 2.205);
+        document.getElementById("wtpd").value = wt_pd;
+    }
+    else if (wt_pd !==""){
+        document.getElementById("wtkg").value = null;
+        wt_kg = wt_pd / 2.205
+        document.getElementById("wtkg").value = wt_kg
+    }
+}
+
+function km2mi(){
+    let ds_km = document.getElementById("dskm").value;
+    let ds_mi = document.getElementById("dsmi").value;
+
+    if (ds_km !== ""){
+        ds_mi = (ds_km / 1.609);
+        document.getElementById("dsmi").value = ds_mi;
+    }
+    else if (ds_mi !==""){
+        ds_km = ds_mi * 1.609
+        document.getElementById("dskm").value = ds_km
+    }
+}
